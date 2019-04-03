2a3105417209a5d80f9dc31375d18218cab3a30d
marcuss.vsc <marcuss.vsc@gmail.com>
2019-04-02 22:11:28 -0300

    Definido index.html como retorno do get

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const http = require('http').createServer(app)
  * @Express Server
  */
 app.get('/', (req, res)=>{
-	res.sendFile(__dirname+'/index.html')
+	res.sendFile(__dirname+'/public/index.html')
 })
 app.use(express.static('public'))
 
