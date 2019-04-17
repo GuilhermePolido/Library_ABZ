f34713d7baed3a6d1cd82d82bddd9087f603823a
GuilhermePolido <gui_bileme@hotmail.com>
2019-04-16 21:22:17 -0300

    Adicionado ngCookies como dependencia, alterado para exigir email e senha no login

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,5 +1,5 @@
 'use strict';
 (function () {
-    angular.module('LIBABZ', ['ngRoute', 'ui.mask', 'ngAnimate', 'ui.bootstrap', 'ngSanitize'])
+    angular.module('LIBABZ', ['ngRoute', 'ngCookies', 'ui.mask', 'ngAnimate', 'ui.bootstrap', 'ngSanitize'])
             .constant('CONFIG', {PATH: 'app/'});
 })();
--- public/app/livro/list/listLivroController.js
+++ public/app/livro/list/listLivroController.js
@@ -2,7 +2,7 @@
 (function () {
     angular.module('LIBABZ')
             .controller('listLivroController', function (
-                $scope, $location, $http, $log, $httpParamSerializer, $timeout, $rootScope
+                $scope, $location, $http, $window, $log, $httpParamSerializer, $timeout, $rootScope, $cookies
                 ) {
                     const path = '/api/livros'
                     let lv = this;
@@ -10,9 +10,12 @@
                     lv.error = false;
                     lv.loading = true;
 
+                    $cookies.put('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imd1aWxoZXJtZXBvbGlkb0BnbWFpbC5jb20iLCJuYW1lIjoiR3VpbGhlcm1lIiwiaWF0IjoxNTU1NDU5MzQzLCJleHAiOjE1NTU1NDU3NDN9._9uXkGJFWBn0hPizgEBaWUu2TcP7lFuv9sYIucv45Aw');
+
                     $http({
                         method: 'GET',
-                        url: path
+                        url: path,
+                        headers: {'x-access-token': $cookies.get('token')}
                     }).then(function succesCallBack(response) {
                         lv.livros = response['data'];
                         lv.error = false;
