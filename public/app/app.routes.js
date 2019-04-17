895f868112cc586488943dff1732a0703b84af28
Geison Pauline <geisonpaulini@hotmail.com>
2019-04-16 21:50:32 -0300

    Criado tela emprestimo

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -11,6 +11,18 @@
                 .when('/livros/new', {
                     templateUrl: CONFIG.PATH + 'livro/new/newLivro.html'
                 })
+                .when('/livros/reserva', {
+                    templateUrl: CONFIG.PATH + 'livro/reserva/reserva.html'
+                })
+                .when('/livros/reserva/:id', {
+                    templateUrl: CONFIG.PATH + 'livro/reserva/reserva.html'
+                })
+                .when('/livros/emprestimo', {
+                    templateUrl: CONFIG.PATH + 'livro/emprestimo/emprestimo.html'
+                })
+                .when('/livros/emprestimo/:id', {
+                    templateUrl: CONFIG.PATH + 'livro/emprestimo/emprestimo.html'
+                })
                 .otherwise({redirectTo: '/'});
 
         // $sceDelegateProvider.resourceUrlWhitelist([
