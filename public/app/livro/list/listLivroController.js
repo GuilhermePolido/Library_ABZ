aee1d667b1cb0c335331a46232c1166617b6ea6a
GuilhermePolido <gui_bileme@hotmail.com>
2019-04-20 00:04:01 -0300

    Iniciado estrutura das telas de edição, detalhes, reserva, devolução e emprestimo de livros

--- public/app/livro/list/listLivroController.js
+++ public/app/livro/list/listLivroController.js
@@ -9,6 +9,9 @@
                     lv.livros = null;
                     lv.error = false;
                     lv.loading = true;
+
+                    //0 - List, 1 - details, 2 - edit, 3 - delete, 4 - reversar, 5 - emprestimo, 6 - devolução.
+                    lv.screen = 0;
                     
                     $http({
                         method: 'GET',
@@ -16,11 +19,34 @@
                         headers: {'x-access-token': $cookies.get('token')}
                     }).then(function succesCallBack(response) {
                         lv.livros = response['data'];
+                        console.log(lv.livros);
                         lv.error = false;
                         lv.loading = false;
                     }, function errorCallBack(response) {
                         lv.error = true;
                         lv.loading = false;
                     });
+
+                    lv.open = (livro) => {
+                        console.log(livro);
+                        lv.screen = 1;
+                        lv.livro = livro;
+                    }
+
+                    lv.edit = (livro) => {
+                        console.log(livro);
+                        lv.screen = 2;
+                        lv.livro = livro;
+                    }
+
+                    lv.delete = (livro) => {
+                        console.log(livro);
+                        lv.livro = livro;
+                    }
+
+                    lv.back = () => {
+                        lv.screen = 0;
+                        lv.livro = null;
+                    }
     });
 })();
